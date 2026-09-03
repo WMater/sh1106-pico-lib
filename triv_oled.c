c54a08ea5357ea66a6a9f88df39707668e1d95ad
wiktormaterlak <materlakw@gmail.com>
2026-09-03 20:37:53 +0200

    error handling

--- triv_oled.c
+++ triv_oled.c
@@ -1,80 +1,13 @@
 #include "triv_oled.h"
+//config
+OledStatus_t cmd(uint8_t cmd, DISPLAY* display){
 
-//struct inits
-void init_display_struct(DISPLAY* display, i2c_inst_t* i2c, uint8_t addr, uint8_t* buff){
-
-    display->i2c = i2c;
-    display->addr = addr;
-    display->buff = buff;
-
-    init_display(display);
-}
-
-void init_sector_struct(SECTOR* sector, int x0, int y0, int height, int width){
-    if(x0 < 0 || x0 >= DISPLAY_WIDTH - 4 || y0 < 0 || y0 >= DISPLAY_HEIGHT) return;
-    sector->x0 = x0;
-    sector->y0 = y0;
-    sector->height = height;
-    sector->width = width;
-    
-    int x1 = x0 + width - 1;
-    int y1 = y0 - height + 1;
-    if(x1 < 0 || x1 >= DISPLAY_WIDTH - 4 || y1 < 0 || y1 >= DISPLAY_HEIGHT) return;
-    sector->x1 = x1;
-    sector->y1 = y1;
-}
-
-//operations
-void cmd(uint8_t cmd, DISPLAY* display){
-
+    if(display->init != OLED_OK) return OLED_ERR_GEN;
     static uint8_t data[2];
     data[0] = SEND_COMMAND;
     data[1] = cmd;
-    i2c_write_blocking(display->i2c, display->addr, data, 2, false);
-}
-
-void write_screen(DISPLAY* display){
-
-    static uint8_t datacmd[DISPLAY_WIDTH + 1];
-    datacmd[0] = SEND_DATA;
-    
-    for(int page = 0; page < 8; page++){
-        
-        memcpy(&datacmd[1], (display->buff + DISPLAY_WIDTH*page), DISPLAY_WIDTH);
-        cmd(SET_PAGE + page, display);
-
-        i2c_write_blocking(display->i2c, display->addr, datacmd, DISPLAY_WIDTH + 1, false);
-    }
-}
-
-void write_sector(SECTOR sector, DISPLAY* display){
-    //updates selected sector only
-
-    //setting helper array containing page data and data control byte
-    static uint8_t pagedata[DISPLAY_WIDTH];
-    pagedata[0] = SEND_DATA;
-    
-    int8_t page = sector.y0 / 8;
-    int8_t last_page = (sector.y0 + 1 - sector.height)  / 8;
-
-    //sending loop
-    while(page >= last_page){
-        
-        //seting up sector starting column as writing start column
-        cmd(SET_PAGE + page, display);
-        cmd(SET_H_COL + ((sector.x0 + ST_LINE) & 0b11110000), display);
-        cmd(SET_L_COL + ((sector.x0 + ST_LINE) & 0b00001111), display);
-
-        memcpy(&pagedata[1], (display->buff + DISPLAY_WIDTH*page + sector.x0), sector.width);
-
-        i2c_write_blocking(display->i2c, display->addr, pagedata, sector.width+1, false);
-
-        page--;
-    }
-        
-    //back to initial starting line (line 2)
-    cmd(SET_L_COL + (ST_LINE & 0b00001111), display);
-    cmd(SET_H_COL + (ST_LINE & 0b11110000), display);
+    if(i2c_write_blocking(display->i2c, display->addr, data, 2, false) == PICO_ERROR_GENERIC) return OLED_ERR_GEN;
+    return OLED_OK;
 }
 
 void clear_buff(DISPLAY* display){
@@ -84,41 +17,177 @@ void clear_buff(DISPLAY* display){
     memset(display->buff, filler, DISPLAY_WIDTH * 8);
 }
 
-void init_display(DISPLAY* display){
+OledStatus_t init_display(DISPLAY* display){
 
+    OledStatus_t status = OLED_OK;
     //screen off
-    cmd(SCREEN_OFF, display);
+    status |= cmd(SCREEN_OFF, display);
 
     //columns addresing policy
-    cmd(SET_COL_W_REV, display);
+    status |= cmd(SET_COL_W_REV, display);
 
     //starting column initialization
-    cmd(SET_L_COL + (ST_LINE & 0b00001111), display);//sh: 0b0010 ssd: 0b0000
-    cmd(SET_H_COL + (ST_LINE & 0b11110000), display);
+    status |= cmd(SET_L_COL + (ST_LINE & 0b00001111), display);//sh: 0b0010 ssd: 0b0000
+    status |= cmd(SET_H_COL + (ST_LINE & 0b11110000), display);
 
     //clear screen
     clear_buff(display);
-    write_screen(display);
+    status |= write_screen(display);
 
     //screen on
-    cmd(SCREEN_ON, display);
+    status |= cmd(SCREEN_ON, display);
+
+    return status;
 }
 
+//structs
+void init_display_struct(DISPLAY* display, i2c_inst_t* i2c, uint8_t addr, uint8_t* buff){
 
-void draw_pixel(int x, int y, DISPLAY* display){
+    display->i2c = i2c;
+    display->addr = addr;
+    display->buff = buff;
+
+    display->init = init_display(display);
+}
+
+void init_sector_struct(SECTOR* sector, int x0, int y0, int height, int width){
+    if((unsigned int)x0 >= DISPLAY_WIDTH - (2*ST_LINE) || (unsigned int)y0 >= DISPLAY_HEIGHT){
+
+        sector->init = OLED_ERR_INIT_FAILED;
+        return;
+    }
+    sector->x0 = x0;
+    sector->y0 = y0;
+    sector->height = height;
+    sector->width = width;
+    
+    int x1 = x0 + width - 1;
+    int y1 = y0 - height + 1;
+    if((unsigned int)x1 >= DISPLAY_WIDTH - 4 || (unsigned int)y1 >= DISPLAY_HEIGHT){
+
+        sector->init = OLED_ERR_INIT_FAILED;
+        return;
+    }
+    sector->x1 = x1;
+    sector->y1 = y1;
+    
+    sector->init = OLED_OK;
+}
+
+void resize_sector(SECTOR* sector, int height, int width){
+
+    int x1 = sector->x0 + width - 1;
+    int y1 = sector->y0 - height + 1;
+    if((unsigned int)x1 >= (DISPLAY_WIDTH - (2*ST_LINE)) || (unsigned int)y1 >= DISPLAY_HEIGHT){
+
+        sector->init = OLED_ERR_INIT_FAILED;
+    }
+
+    sector->height = height;
+    sector->width = width;
+    sector->x1 = x1;
+    sector->y1 = y1;
+}
+
+void move_sector(SECTOR* sector, int x0, int y0){
+    //moves it only logicly, should be cleared before moving
+    //for moving filled sectors it is recomended to write a simple helper function f.eg.
+    //clear_sector()
+    //move_sector()
+    //draw to sector
+    //write_sector
+    int x1 = x0 + sector->width - 1;
+    int y1 = y0 - sector->height + 1;
+
+    if((unsigned int)x0 >= (DISPLAY_WIDTH - (2*ST_LINE)) || (unsigned int)y0 >= DISPLAY_HEIGHT || (unsigned int)x1 >= (DISPLAY_WIDTH - (2*ST_LINE)) || (unsigned int)y1 >= DISPLAY_HEIGHT){
+
+        sector->init = OLED_ERR_INIT_FAILED;
+    }
+
+    sector->x0 = x0;
+    sector->y0 = y0;
+    
+    sector->x1 = x1;
+    sector->y1 = y1;
+}
+
+//sending buffor
+OledStatus_t write_screen(DISPLAY* display){
+
+    if(display->init != OLED_OK) return OLED_ERR_INIT_FAILED;
+
+    OledStatus_t status = OLED_OK;
+    static uint8_t datacmd[DISPLAY_WIDTH + 1];
+    datacmd[0] = SEND_DATA;
+    
+    for(int page = 0; page < 8; page++){
+        
+        memcpy(&datacmd[1], (display->buff + DISPLAY_WIDTH*page), DISPLAY_WIDTH);
+        status |= cmd(SET_PAGE + page, display);
+
+        if(i2c_write_blocking(display->i2c, display->addr, datacmd, DISPLAY_WIDTH + 1, false) == PICO_ERROR_GENERIC) status |= OLED_ERR_GEN;
+    }
+
+    return status;
+}
+
+OledStatus_t write_sector(SECTOR* sector, DISPLAY* display){
+    //updates selected sector only
+
+    if(sector->init != OLED_OK || display->init != OLED_OK) return OLED_ERR_INIT_FAILED;
+    OledStatus_t status = OLED_OK;
+
+    //setting helper array containing page data and data control byte
+    static uint8_t pagedata[DISPLAY_WIDTH];
+    pagedata[0] = SEND_DATA;
+    
+    int8_t page = sector->y0 / 8;
+    int8_t last_page = sector->y1  / 8;
+
+    //sending loop
+    while(page >= last_page){
+        
+        //seting up sector starting column as writing start column
+        status |= cmd(SET_PAGE + page, display);
+        status |= cmd(SET_H_COL + (((sector->x0 + ST_LINE) & 0b11110000)>>4), display);
+        status |= cmd(SET_L_COL + ((sector->x0 + ST_LINE) & 0b00001111), display);
+
+        memcpy(&pagedata[1], (display->buff + DISPLAY_WIDTH*page + sector->x0), sector->width);
+
+        if(i2c_write_blocking(display->i2c, display->addr, pagedata, sector->width+1, false) == PICO_ERROR_GENERIC) status |= OLED_ERR_GEN;
+
+        page--;
+    }
+        
+    //back to initial starting line (line 2)
+    status |= cmd(SET_L_COL + (ST_LINE & 0b00001111), display);
+    status |= cmd(SET_H_COL + ((ST_LINE & 0b11110000)>>4), display);
+
+    return status;
+}
+
+//filling buff
+OledStatus_t draw_pixel(int x, int y, DISPLAY* display){
     
     int8_t page = y / 8;
     uint8_t pixel = 1 << (y % 8);
 
-    if((unsigned int)y < DISPLAY_HEIGHT && (unsigned int)x < DISPLAY_WIDTH){
+    if(display->init != OLED_OK) return OLED_ERR_INIT_FAILED;
+
+    if((unsigned int)y >= DISPLAY_HEIGHT || (unsigned int)x >= (DISPLAY_WIDTH - (2*ST_LINE))){
         
-        *(display->buff + DISPLAY_WIDTH*page + x) |= pixel;
+        return OLED_WAR_OUT_OF_BOUND;
     }
+
+    *(display->buff + DISPLAY_WIDTH*page + x) |= pixel;
+    return OLED_OK;
 }
 
 
-void draw_circle(int x, int y, int radius, DISPLAY* display){ //midpoint for circle, takes central point
+OledStatus_t draw_circle(int x, int y, int radius, DISPLAY* display){ //midpoint for circle, takes central point
     
+    OledStatus_t status = OLED_OK;
+
     int xi = 0;
     int yi = radius;
 
@@ -126,17 +195,17 @@ void draw_circle(int x, int y, int radius, DISPLAY* display){ //midpoint for cir
         
     while(xi <= yi){
             
-        draw_pixel(x + xi, y + yi, display);
-        draw_pixel(x + xi, y - yi, display);
+        status |= draw_pixel(x + xi, y + yi, display);
+        status |= draw_pixel(x + xi, y - yi, display);
 
-        draw_pixel(x - xi, y + yi, display);
-        draw_pixel(x - xi, y - yi, display);
+        status |= draw_pixel(x - xi, y + yi, display);
+        status |= draw_pixel(x - xi, y - yi, display);
 
-        draw_pixel(x + yi, y + xi, display);
-        draw_pixel(x - yi, y + xi, display);
+        status |= draw_pixel(x + yi, y + xi, display);
+        status |= draw_pixel(x - yi, y + xi, display);
 
-        draw_pixel(x + yi, y - xi, display);
-        draw_pixel(x - yi, y - xi, display);
+        status |= draw_pixel(x + yi, y - xi, display);
+        status |= draw_pixel(x - yi, y - xi, display);
 
         if(d >= 0){
 
@@ -149,10 +218,14 @@ void draw_circle(int x, int y, int radius, DISPLAY* display){ //midpoint for cir
 
         xi++;
     }
+
+    return status;
 }
 
-void draw_line(int x0, int y0, int x1, int y1, DISPLAY* display){
+OledStatus_t draw_line(int x0, int y0, int x1, int y1, DISPLAY* display){
     
+    OledStatus_t status = OLED_OK;
+
     int dx = abs(x1 - x0);
     int dy = -abs(y1 - y0);
 
@@ -165,7 +238,7 @@ void draw_line(int x0, int y0, int x1, int y1, DISPLAY* display){
     int y = y0;
     while(!(x == x1 && y == y1)){
         
-        draw_pixel(x, y, display);
+        status |= draw_pixel(x, y, display);
 
         int d2 = 2*d;
         if(d2 >= dy){
@@ -177,12 +250,15 @@ void draw_line(int x0, int y0, int x1, int y1, DISPLAY* display){
             y += ystep;
         }
     }
-    draw_pixel(x, y, display);
+    status |= draw_pixel(x, y, display);
+
+    return status;
 }
 
 
-void draw_letter(int x0, int y0, char letter, DISPLAY* display){
+OledStatus_t draw_letter(int x0, int y0, char letter, DISPLAY* display){
 
+    if(letter > '~') return OLED_ERR_GEN;
     int8_t shift = y0 % 8 + 1;
     int8_t top_page, bot_page;
 
@@ -196,23 +272,28 @@ void draw_letter(int x0, int y0, char letter, DISPLAY* display){
             *(display->buff + (DISPLAY_WIDTH*bot_page) + x0 + i) |= ascii[letter - OFFSET][i] << shift;
             *(display->buff + (DISPLAY_WIDTH*top_page) + x0 + i) |= ascii[letter - OFFSET][i] >> (8-shift);
         }
+        return OLED_OK;
+    }else{
+        return OLED_ERR_OUT_OF_BOUND;
     }
 }
 
 
-void draw_bitmap(SECTOR sector, const BITMAP bitmap, DISPLAY* display){
+OledStatus_t draw_bitmap(SECTOR* sector, const BITMAP bitmap, DISPLAY* display){
 
-    if(sector.height != bitmap.bitmap_height || sector.width != bitmap.bitmap_width) return;
+    if(sector->init != OLED_OK || display->init != OLED_OK) return OLED_ERR_INIT_FAILED;
 
-    int8_t top_page = sector.y0 / 8;
-    int8_t bot_page = sector.y1 / 8;
-    if(top_page >= 8 || bot_page < 0) return;
+    if(sector->height != bitmap.bitmap_height || sector->width != bitmap.bitmap_width) return OLED_ERR_BITMAP_NFIT;
 
-    int shift = 8 - ((sector.y0 + 1) % 8);
+    int8_t top_page = sector->y0 / 8;
+    int8_t bot_page = sector->y1 / 8;
+    if(top_page >= 8 || bot_page < 0) return OLED_ERR_OUT_OF_BOUND;
+
+    int shift = 8 - ((sector->y0 + 1) % 8);
     if(shift == 8) shift = 0;
     
     int bitmap_index = 0;
-    for(int x = sector.x0; x <= sector.x1; x++){
+    for(int x = sector->x0; x <= sector->x1; x++){
 
         int page = top_page;
         for(int i = 0; i < ((bitmap.bitmap_height + 7)/8); i++){
@@ -225,22 +306,38 @@ void draw_bitmap(SECTOR sector, const BITMAP bitmap, DISPLAY* display){
             bitmap_index++;
         }
     }
+    return OLED_OK;
 }
 
-SECTOR draw_text(int x0, int y0, char* string, int len, DISPLAY* display){
+OledStatus_t draw_text_with_init(int x0, int y0, char* string, size_t len, SECTOR* sector, DISPLAY* display){
 
+    OledStatus_t status = OLED_OK;
     for(int i = 0; *(string+i) != '\0'; i++){
 
-        draw_letter(x0 + 6*i, y0, *(string+i), display);
+        status |= draw_letter(x0 + 6*i, y0, *(string+i), display);
         
     }
     
-    SECTOR sector;
-    init_sector_struct(&sector, x0, y0, 8, len+(len*5)-1);
-    return sector;
+    init_sector_struct(sector, x0, y0, 8, len+(len*5)-1);
+    status |= sector->init;
+    return status;
 }
 
-SECTOR draw_int(int x0, int y0, int integer, DISPLAY* display){
+OledStatus_t draw_text(char* string, int len, SECTOR* sector, DISPLAY* display){
+    if(len+(len*5)-1 > sector->width) return OLED_ERR_OUT_OF_BOUND;
+
+    OledStatus_t status = OLED_OK;
+    for(int i = 0; *(string+i) != '\0'; i++){
+
+        status |= draw_letter(sector->x0 + 6*i, sector->y0, *(string+i), display);
+    }
+
+    return status;
+}
+
+OledStatus_t draw_int_with_init(int x0, int y0, int integer, SECTOR* sector, DISPLAY* display){
+
+    if(integer < 0) return OLED_ERR_NEGNUM;
 
     int n = 0;
     int tmp = integer;
@@ -260,23 +357,47 @@ SECTOR draw_int(int x0, int y0, int integer, DISPLAY* display){
         integer /= 10;
     }
 
-    return draw_text(x0, y0, string, n, display);
+    return draw_text_with_init(x0, y0, string, n, sector, display);
 }
 
-void clear_sector(SECTOR sector, DISPLAY* display){
-    
-    int8_t page = sector.y0 / 8;
-    int8_t last_page = sector.y1 / 8;
+OledStatus_t draw_int(int integer, SECTOR* sector, DISPLAY* display){
 
-    int8_t top_shift = 8 - ((sector.y0 + 1) % 8); //sector.y0 % 8;
+    if(integer < 0) return OLED_ERR_NEGNUM;
+    int n = 0;
+    int tmp = integer;
+
+    //prepares string for displaying
+    do{
+        n++;
+        tmp /= 10;
+    }while (tmp > 0);
+
+    static char string[12];
+    string[n] = '\0';
+
+    for(int i = 0; i < n; i++){
+
+        string[n - 1 - i] = integer % 10 + '0';
+        integer /= 10;
+    }
+
+    return draw_text(string, n, sector, display);
+}
+
+void clear_sector(SECTOR* sector, DISPLAY* display){
+    
+    int8_t page = sector->y0 / 8;
+    int8_t last_page = sector->y1 / 8;
+
+    int8_t top_shift = 8 - ((sector->y0 + 1) % 8); //sector.y0 % 8;
     if(top_shift == 8) top_shift = 0;
-    int8_t bottom_shift = sector.y1 % 8;
+    int8_t bottom_shift = sector->y1 % 8;
 
     //clears top and bottom line of the sector
-    for(int i = 0; i < sector.width; i++){
+    for(int i = 0; i < sector->width; i++){
 
-        *(display->buff + DISPLAY_WIDTH*(last_page) + sector.x0 + i) &= ~(0b11111111 << bottom_shift);
-        *(display->buff + DISPLAY_WIDTH*page + sector.x0 + i) &= ~(0b11111111 >> top_shift);
+        *(display->buff + DISPLAY_WIDTH*(last_page) + sector->x0 + i) &= ~(0b11111111 << bottom_shift);
+        *(display->buff + DISPLAY_WIDTH*page + sector->x0 + i) &= ~(0b11111111 >> top_shift);
     }
 
     //clears whats left
@@ -284,6 +405,6 @@ void clear_sector(SECTOR sector, DISPLAY* display){
 
     for(int i = page-1; i > last_page; i--){
 
-        memset((display->buff+DISPLAY_WIDTH*i + sector.x0), filler, sector.width);
+        memset((display->buff+DISPLAY_WIDTH*i + sector->x0), filler, sector->width);
     }
 }
--- triv_oled.h
+++ triv_oled.h
@@ -29,28 +29,30 @@
 typedef enum{
     OLED_OK,
     //warnings
-    OLED_WAR_PIXEL_OUT_OF_BOUND,
-    OLED_WAR_LINE_OUT_OF_BOUND,
-    OLED_WAR_CIRCLE_OUT_OF_BOUND,
+    OLED_WAR_OUT_OF_BOUND,
     //errors
-    OLED_ERR_SECTOR_INIT_FAILED,
+    OLED_ERR_INIT_FAILED,
     OLED_ERR_BITMAP_NFIT,
-    OLED_ERR_TEXT_OUT_OF_BOUND
+    OLED_ERR_OUT_OF_BOUND,
+    OLED_ERR_GEN,
+    OLED_ERR_NEGNUM
 }OledStatus_t;
 
 typedef struct{
     i2c_inst_t *i2c;
     uint8_t addr;
     uint8_t* buff;
+    OledStatus_t init;
 }DISPLAY;
 
 typedef struct{
-    unsigned int x0;
-    unsigned int y0;
-    unsigned int x1;
-    unsigned int y1;
-    unsigned int height;
-    unsigned int width;
+    int x0;
+    int y0;
+    int x1;
+    int y1;
+    int height;
+    int width;
+    OledStatus_t init;
 }SECTOR;
 
 typedef struct{
@@ -59,34 +61,31 @@ typedef struct{
     const uint8_t *bitmap;
 }BITMAP;
 
+OledStatus_t cmd(uint8_t cmd, DISPLAY* display);
+void clear_buff(DISPLAY* display);
+
 void init_display_struct(DISPLAY* display, i2c_inst_t* i2c, uint8_t addr, uint8_t* buff);
 
 void init_sector_struct(SECTOR* sector, int x0, int y0, int height, int width);
+void resize_sector(SECTOR* sector, int height, int width);
+void move_sector(SECTOR* sector, int x0, int y0);
 
-void cmd(uint8_t cmd, DISPLAY* display);
+OledStatus_t write_screen(DISPLAY* display);
+OledStatus_t write_sector(SECTOR* sector, DISPLAY* display);
 
-void write_screen(DISPLAY* display);
 
-void write_sector(SECTOR sector, DISPLAY* display);
+OledStatus_t draw_pixel(int x, int y, DISPLAY* display);
+OledStatus_t draw_circle(int x, int y, int radius, DISPLAY* display);
+OledStatus_t draw_line(int x0, int y0, int x1, int y1, DISPLAY* display);
 
-void clear_buff(DISPLAY* display);
+OledStatus_t draw_letter(int x0, int y0, char letter, DISPLAY* display);
+OledStatus_t draw_text_with_init(int x0, int y0, char* string, size_t len, SECTOR* sector, DISPLAY* display);
+OledStatus_t draw_text(char* string, int len, SECTOR* sector, DISPLAY* display);
+OledStatus_t draw_int_with_init(int x0, int y0, int integer, SECTOR* sector, DISPLAY* display);
+OledStatus_t draw_int(int integer, SECTOR* sector, DISPLAY* display);
 
-void init_display(DISPLAY* display);
+OledStatus_t draw_bitmap(SECTOR* sector, const BITMAP bitmap, DISPLAY* display);
 
-void draw_pixel(int x, int y, DISPLAY* display);
-
-void draw_circle(int x, int y, int radius, DISPLAY* display);
-
-void draw_line(int x0, int y0, int x1, int y1, DISPLAY* display);
-
-void draw_letter(int x0, int y0, char letter, DISPLAY* display);
-
-void draw_bitmap(SECTOR sector, const BITMAP bitmap, DISPLAY* display);
-
-SECTOR draw_text(int x0, int y0, char* string, int len, DISPLAY* display);
-
-SECTOR draw_int(int x0, int y0, int integer, DISPLAY* display);
-
-void clear_sector(SECTOR sector, DISPLAY* display);
+void clear_sector(SECTOR* sector, DISPLAY* display);
 
 #endif
